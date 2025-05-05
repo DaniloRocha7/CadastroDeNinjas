e4f31a06ff21c14fea14fa0e6719bc585d7f2164
DaniloRocha7 <danilorocha4@icloud.com>
2025-05-05 00:19:07 -0300

    feat: Crianção do DTO e Mapper

---
+++ src/main/java/Danilo/CadastroDeNinja/Ninjas/Controller/NinjaMapper.java
@@ -0,0 +1,4 @@
+package Danilo.CadastroDeNinja.Ninjas.Controller;
+
+public class NinjaMapper {
+}
--- src/main/java/Danilo/CadastroDeNinja/Ninjas/Controller/NinjaService.java
+++ src/main/java/Danilo/CadastroDeNinja/Ninjas/Controller/NinjaService.java
@@ -3,18 +3,47 @@ package Danilo.CadastroDeNinja.Ninjas.Controller;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
 
-    private NinjaRepository ninjarepository;
+    private NinjaRepository ninjaRepository;
 
     public NinjaService(NinjaRepository ninjarepository) {
-        this.ninjarepository = ninjarepository;
+        this.ninjaRepository = ninjarepository;
     }
 
     //Listar todos os meus ninjas
     public List<NinjaModel> listNinjas(){
-        return ninjarepository.findAll();
+        return ninjaRepository.findAll();
     }
+
+    //Listar todos os meus ninjas por ID
+    public NinjaModel listNinjaById(Long id){
+        Optional<NinjaModel> ninjaById = ninjaRepository.findById(id);
+        return ninjaById.orElse(null);
+    }
+
+    //Criar um novo ninja
+    public NinjaModel generateNinja(NinjaModel ninja){
+        return ninjaRepository.save(ninja);
+    }
+
+    //Deletar o ninja - Tem que ser um metodo VOID
+    public void deleteNinjaById(Long id){
+        ninjaRepository.deleteById(id);
+    }
+
+    //Atualizar ninja
+    public NinjaModel updateNinja(Long id, NinjaModel ninjaUpdate){
+
+        if (ninjaRepository.existsById(id)){
+            ninjaUpdate.setId(id);
+            return ninjaRepository.save(ninjaUpdate);
+        }
+
+        return null;
+    }
+
 }
